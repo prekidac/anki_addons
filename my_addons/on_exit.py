fd187492c353f1b356faff1625e686b455ac0481
prekidac <al.slavisa@gmail.com>
2020-11-24 09:01:56 +0100

    fixet

--- my_addons/on_exit.py
+++ my_addons/on_exit.py
@@ -5,22 +5,20 @@ def unloadProfileAndExit(self) -> None:
     with open('/tmp/novih', 'w') as f:
         f.write('pr_kartica=' + str(new[0]))
 
-    # FIXME: execute to all
-    all_sus_nid = self.col.db.execute("""select nid from cards where queue == -1 group by nid""")
-    today_cid = self.col.db.execute("""select cid from revlog where id > ? """, (self.col.sched.dayCutoff-86400)*1000)
+    all_sus_nid = self.col.db.list("""select nid from cards where queue == -1 group by nid""")
+    today_cid = self.col.db.list("""select cid from revlog where id > ? """, (self.col.sched.dayCutoff-86400)*1000)
     today_nid = []
     for cid in today_cid:
-        today_nid.append(self.col.db.execute(""" select nid from cards where id == ? """, cid[0])[0])
-    print(today_cid, today_nid)
-
+        today_nid.append(self.col.db.scalar(""" select nid from cards where id == ? """, cid))
     to_sus_nid = []
     for nid in all_sus_nid:
-        nid_queues = self.col.db.execute("""select queue from cards where nid == """ + str(nid[0]))
+        nid_queues = self.col.db.list("""select queue from cards where nid == """ + str(nid))
         for queue in nid_queues:
-            if queue[0] != -1 or nid in today_nid:
+            if queue != -1:
                 break
         else:
-            to_sus_nid.append(nid[0])
+            if nid not in today_nid:
+                to_sus_nid.append(nid)
     self.col.remove_notes(to_sus_nid)
 
     self.unloadProfile(self.cleanupAndExit)
