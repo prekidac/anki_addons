19f1d9a3e9c267ec608af9c98e7e92544b5bab46
prekidac <al.slavisa@gmail.com>
2020-11-24 07:49:23 +0100

    on exit delete old sus cards

--- my_addons/__init__.py
+++ my_addons/__init__.py
@@ -1,7 +1,7 @@
 from . import shortcuts         # Enter/Tab review
 from . import default_conf      # restore to default configuration
 from . import update_review_ivl # no fuzz and suspend card on maxIvl
-from . import on_exit           # remove learned cards
+from . import on_exit           # remove learned and # of new
 from . import unbury_block      # block unBury button
-from . import letter_limit      # study letter count limit
+#from . import letter_limit      # study letter count limit
 #from . import time_limit        # study time limit
--- my_addons/on_exit.py
+++ my_addons/on_exit.py
@@ -5,18 +5,23 @@ def unloadProfileAndExit(self) -> None:
     with open('/tmp/novih', 'w') as f:
         f.write('pr_kartica=' + str(new[0]))
 
-    all_nid = self.col.db.execute("""select nid from cards
-                                  where queue == -1 group by nid""")
-    sus_nid = []
-    for nid in all_nid:
-        nid_queues = self.col.db.execute("""select queue from cards
-                                    where nid == """ + str(nid[0]))
+    # FIXME: execute to all
+    all_sus_nid = self.col.db.execute("""select nid from cards where queue == -1 group by nid""")
+    today_cid = self.col.db.execute("""select cid from revlog where id > ? """, (self.col.sched.dayCutoff-86400)*1000)
+    today_nid = []
+    for cid in today_cid:
+        today_nid.append(self.col.db.execute(""" select nid from cards where id == ? """, cid[0])[0])
+    print(today_cid, today_nid)
+
+    to_sus_nid = []
+    for nid in all_sus_nid:
+        nid_queues = self.col.db.execute("""select queue from cards where nid == """ + str(nid[0]))
         for queue in nid_queues:
-            if queue[0] != -1:
+            if queue[0] != -1 or nid in today_nid:
                 break
         else:
-            sus_nid.append(nid[0])
-    self.col.remove_notes(sus_nid)
+            to_sus_nid.append(nid[0])
+    self.col.remove_notes(to_sus_nid)
 
     self.unloadProfile(self.cleanupAndExit)
 
