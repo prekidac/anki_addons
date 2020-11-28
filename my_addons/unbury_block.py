0f6fcc978ab880ac66e1fdbdbd9e889057a4655b
prekidac <al.slavisa@gmail.com>
2020-11-28 08:53:36 +0100

    gui button block

--- my_addons/unbury_block.py
+++ my_addons/unbury_block.py
@@ -1,12 +1,8 @@
-from anki.sched import Scheduler
+from aqt.overview import Overview
 
 def unbury_block(func):
     def wrapper(*args, **kwargs):
         pass
     return wrapper
 
-# unburyCardsForDeck --> unbury_cards_in_current_deck
-# u buducnosti ce biti izmena
-
-Scheduler.unburyCardsForDeck = unbury_block(Scheduler.unburyCardsForDeck) 
-Scheduler.unburyCards = unbury_block(Scheduler.unburyCards)
+Overview.onUnbury = unbury_block(Overview.onUnbury) 
