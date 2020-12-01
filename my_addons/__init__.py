1340db4b7bbd094171e812e6e04627699349c230
prekidac <al.slavisa@gmail.com>
2020-12-01 12:05:33 +0100

    time limit del

--- my_addons/__init__.py
+++ my_addons/__init__.py
@@ -3,7 +3,4 @@ from . import default_conf      # restore to default configuration
 from . import update_review_ivl # no fuzz and suspend card on maxIvl
 from . import on_exit           # remove learned and # of new
 from . import unbury_block      # block unBury button
-
-# ili jedan ili drugi limit
 from . import letter_limit      # study letter count limit
-#from . import time_limit        # study time limit
