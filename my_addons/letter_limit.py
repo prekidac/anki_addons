5b2c0ee93767ad59ee84563e5f89ba8748233cf1
prekidac <al.slavisa@gmail.com>
2020-12-03 07:59:07 +0100

    format

--- my_addons/letter_limit.py
+++ my_addons/letter_limit.py
@@ -4,32 +4,39 @@ from aqt.reviewer import Reviewer
 from aqt.utils import tooltip
 from anki.utils import stripHTML
 from anki.collection import Collection
-import re, html, copy
+import re
+import html
+import copy
 import time
 
 try:
-    with open('/tmp/norma', 'r') as norma:
+    with open("/tmp/norma", "r") as norma:
         limit = int(norma.readline().strip())
 except:
     print("Nema normu")
-    limit = 100 
+    limit = 100
 
-KOEF = 10 # reci/% energije
-limit = limit * KOEF 
+KOEF = 10  # reci/% energije
+limit = limit * KOEF
+
 
 def reached_timebox_wrapper(func) -> callable:
     def wrapper(self, *args, **kwargs):
-        today_cards = self.db.list("""select cid from revlog
-            where id > ? """, (self.sched.dayCutoff-86400)*1000)
+        today_cards = self.db.list(
+            "select cid from revlog where id > ?", (self.sched.dayCutoff-86400)*1000)
 
         suma = 0
         for card in today_cards:
-            field_num = self.db.scalar("""select ord from cards where id == ? """, card)
-            nid = self.db.scalar("""select nid from cards where id == ? """, card)
-            fields = self.db.scalar("""select flds from notes where id == ?""", nid)
+            field_num = self.db.scalar(
+                "select ord from cards where id == ?", card)
+            nid = self.db.scalar(
+                "select nid from cards where id == ?", card)
+            fields = self.db.scalar(
+                "select flds from notes where id == ?", nid)
 
             try:
-                answer = answer_from_fields(field_num, self.media.strip(fields))
+                answer = answer_from_fields(
+                    field_num, self.media.strip(fields))
                 suma += letter_count(answer)
             except:
                 pass
@@ -41,19 +48,24 @@ def reached_timebox_wrapper(func) -> callable:
         return func(self, *args, **kwargs)
     return wrapper
 
+
 def start_timebox_wrapper(func) -> callable:
     def wrapper(self, *args, **kwargs):
-        today_cards = self.db.list("""select cid from revlog
-            where id > ? """, (self.sched.dayCutoff-86400)*1000)
-        
+        today_cards = self.db.list(
+            "select cid from revlog where id > ?", (self.sched.dayCutoff-86400)*1000)
+
         suma = 0
         for card in today_cards:
-            field_num = self.db.scalar("""select ord from cards where id == ? """, card)
-            nid = self.db.scalar("""select nid from cards where id == ? """, card)
-            fields = self.db.scalar("""select flds from notes where id == ?""", nid)
+            field_num = self.db.scalar(
+                "select ord from cards where id == ?", card)
+            nid = self.db.scalar(
+                "select nid from cards where id == ?", card)
+            fields = self.db.scalar(
+                "select flds from notes where id == ?", nid)
 
             try:
-                answer = answer_from_fields(field_num, self.media.strip(fields))
+                answer = answer_from_fields(
+                    field_num, self.media.strip(fields))
                 suma += letter_count(answer)
             except:
                 pass
@@ -63,16 +75,18 @@ def start_timebox_wrapper(func) -> callable:
         return func(self, *args, **kwargs)
     return wrapper
 
+
 def letter_count(match_list: list) -> int:
     num = 0
     for match in match_list:
         num += len(match)
     return num
 
+
 def answer_from_fields(field_num: int, fields: str) -> list:
-    """
+    "
     Remove HTML tags from note
-    """
+    "
     # prepisano iz auto_rate_answer addon-a
     fields = re.sub("(\n|<br ?/?>|</?div>)+", " ", fields)
     fields = stripHTML(fields)
@@ -93,23 +107,28 @@ def answer_from_fields(field_num: int, fields: str) -> list:
             pass
     return match_list
 
+
 def moveToState_wrapper(func: callable) -> callable:
     def wrapper(self, state: str, *args, **kwargs):
-        today_cards = self.col.db.list("""select cid from revlog
-            where id > ? """, (self.col.sched.dayCutoff-86400)*1000)
-        
+        today_cards = self.col.db.list(
+            "select cid from revlog where id > ?", (self.col.sched.dayCutoff-86400)*1000)
+
         suma = 0
         for card in today_cards:
-            field_num = self.col.db.scalar("""select ord from cards where id == ? """, card)
-            nid = self.col.db.scalar("""select nid from cards where id == ? """, card)
-            fields = self.col.db.scalar("""select flds from notes where id == ?""", nid)
+            field_num = self.col.db.scalar(
+                "select ord from cards where id == ?", card)
+            nid = self.col.db.scalar(
+                "select nid from cards where id == ?", card)
+            fields = self.col.db.scalar(
+                "select flds from notes where id == ?", nid)
 
             try:
-                answer = answer_from_fields(field_num, self.col.media.strip(fields))
+                answer = answer_from_fields(
+                    field_num, self.col.media.strip(fields))
                 suma += letter_count(answer)
             except:
                 pass
-            
+
         if suma >= limit:
             if state == "overview":
                 state = "deckBrowser"
@@ -118,19 +137,24 @@ def moveToState_wrapper(func: callable) -> callable:
         return func(self, state, *args, **kwargs)
     return wrapper
 
+
 def nextCard_wrapper(func: callable) -> callable:
     def wrapper(self, *args, **kwargs):
-        today_cards = self.mw.col.db.list("""select cid from revlog
-            where id > ? """, (self.mw.col.sched.dayCutoff-86400)*1000)
-        
+        today_cards = self.mw.col.db.list(
+            "select cid from revlog where id > ?", (self.mw.col.sched.dayCutoff-86400)*1000)
+
         suma = 0
         for card in today_cards:
-            field_num = self.mw.col.db.scalar("""select ord from cards where id == ? """, card)
-            nid = self.mw.col.db.scalar("""select nid from cards where id == ? """, card)
-            fields = self.mw.col.db.scalar("""select flds from notes where id == ?""", nid)
+            field_num = self.mw.col.db.scalar(
+                "select ord from cards where id == ?", card)
+            nid = self.mw.col.db.scalar(
+                "select nid from cards where id == ?", card)
+            fields = self.mw.col.db.scalar(
+                "select flds from notes where id == ?", nid)
 
             try:
-                answer = answer_from_fields(field_num, self.mw.col.media.strip(fields))
+                answer = answer_from_fields(
+                    field_num, self.mw.col.media.strip(fields))
                 suma += letter_count(answer)
             except:
                 pass
@@ -142,19 +166,24 @@ def nextCard_wrapper(func: callable) -> callable:
         return func(self, *args, **kwargs)
     return wrapper
 
+
 def stampaj_zadnju(self, *args) -> None:
-    """Stampa zadnji odgovor i ukupnu sumu slova"""
-    today_cards = self.mw.col.db.list("""select cid from revlog
-        where id > ? """, (self.mw.col.sched.dayCutoff-86400)*1000)
-    
+    "Stampa zadnji odgovor i ukupnu sumu slova"
+    today_cards = self.mw.col.db.list(
+        "select cid from revlog where id > ?", (self.mw.col.sched.dayCutoff-86400)*1000)
+
     suma = 0
     for card in today_cards:
-        field_num = self.mw.col.db.scalar("""select ord from cards where id == ? """, card)
-        nid = self.mw.col.db.scalar("""select nid from cards where id == ? """, card)
-        fields = self.mw.col.db.scalar("""select flds from notes where id == ?""", nid)
+        field_num = self.mw.col.db.scalar(
+            "select ord from cards where id == ?", card)
+        nid = self.mw.col.db.scalar(
+            "select nid from cards where id == ?", card)
+        fields = self.mw.col.db.scalar(
+            "select flds from notes where id == ?", nid)
 
         try:
-            answer = answer_from_fields(field_num, self.mw.col.media.strip(fields))
+            answer = answer_from_fields(
+                field_num, self.mw.col.media.strip(fields))
             suma += letter_count(answer)
         except:
             pass
@@ -163,6 +192,7 @@ def stampaj_zadnju(self, *args) -> None:
         print(answer, suma)
     except:
         print("Nema danasnjih kartica")
+
 
 gui_hooks.reviewer_did_answer_card.append(stampaj_zadnju)
 AnkiQt.moveToState = moveToState_wrapper(AnkiQt.moveToState)
