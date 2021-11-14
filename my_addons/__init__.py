dda51729f6bc793b28083524b1fa6dac8d20bd4f
slavujko <al.slavisa@gmail.com>
2021-11-14 17:46:09 +0100

    ➕ add(main): easy new card

--- my_addons/__init__.py
+++ my_addons/__init__.py
@@ -6,3 +6,4 @@ from . import energy            # study limit
 from . import auto_rate_typed_answer
 from . import suspend_on_max    # suspend on max interval
 from . import write_new_cards 
+from . import easy_new_card     # answer button 4 when you know new card
---
+++ my_addons/easy_new_card.py
@@ -0,0 +1,10 @@
+from aqt import gui_hooks
+
+
+def easy_new_card(ease_tuple, reviewer, card):
+    if ease_tuple[1] > 1 and card.queue == 0:
+        return (True, 4)
+    return ease_tuple
+
+
+gui_hooks.reviewer_will_answer_card.append(easy_new_card)
