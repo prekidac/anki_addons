020c0dc5b90fe40ae32b82cba4605bf7a4b2b275
slavujko <al.slavisa@gmail.com>
2021-11-17 17:02:39 +0100

    ➕ add(main): display easy card

--- my_addons/easy_new_card.py
+++ my_addons/easy_new_card.py
@@ -3,6 +3,7 @@ from aqt import gui_hooks
 
 def easy_new_card(ease_tuple, reviewer, card):
     if ease_tuple[1] > 1 and card.queue == 0:
+        print("-- Easy card: ", end="")
         return (True, 4)
     return ease_tuple
 
