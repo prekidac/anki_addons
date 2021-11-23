204a0a104ef08256f701e5027d9473e118d6de99
slavujko <al.slavisa@gmail.com>
2021-11-23 12:31:31 +0100

    ➕ add(main): push hard cards

--- my_addons/answer_modify.py
+++ my_addons/answer_modify.py
@@ -3,8 +3,12 @@ from aqt import gui_hooks
 
 def easy_new_card(ease_tuple, reviewer, card):
     if ease_tuple[1] > 1 and card.queue == 0:
+        # new card and you know it - answer 4
         print("-- Easy card: ", end="")
         return (True, 4)
+    elif card.reps >= 10:
+        print("-- Hard card: ", end="")
+        return (True, 3)
     return ease_tuple
 
 
