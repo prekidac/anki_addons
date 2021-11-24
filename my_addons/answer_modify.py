2dc51c08c5b45c5d4b12894dbbfd5bb4544f70fc
slavujko <al.slavisa@gmail.com>
2021-11-24 06:55:22 +0100

    ➕ add(main): only if you fail hard card

--- my_addons/answer_modify.py
+++ my_addons/answer_modify.py
@@ -6,7 +6,8 @@ def easy_new_card(ease_tuple, reviewer, card):
         # new card and you know it - answer 4
         print("-- Easy card: ", end="")
         return (True, 4)
-    elif card.reps >= 10:
+    elif ease_tuple[1] == 1 and card.reps >= 10:
+        # if you fail card with >10 reps
         print("-- Hard card: ", end="")
         return (True, 3)
     return ease_tuple
