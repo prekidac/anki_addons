5f37eb63ee73d3b94bbebb5c54d701339fd67c44
slavujko <al.slavisa@gmail.com>
2021-12-08 21:52:53 +0100

    🔧 change(main): remove all suspended

--- my_addons/remove_suspended.py
+++ my_addons/remove_suspended.py
@@ -4,27 +4,20 @@ import subprocess
 
 def unloadProfileAndExit_wrapper(func) -> callable:
     def wrapper(self):
-        all_sus_nid = self.col.db.list(
+        sus_nid = self.col.db.list(
             "select nid from cards where queue == -1 group by nid")
-        today_cid = self.col.db.list(
-            "select cid from revlog where id > ? ", (self.col.sched.dayCutoff-86400)*1000)
-        today_nid = []
-        for cid in today_cid:
-            today_nid.append(self.col.db.scalar(
-                "select nid from cards where id == ?", cid))
-        to_sus_nid = []
+        remove_nids = []
         num_of_del = 0
-        for nid in all_sus_nid:
+        for nid in sus_nid:
             nid_queues = self.col.db.list(
-                "select queue from cards where nid == " + str(nid))
+                "select queue from cards where nid == ?", nid)
             for queue in nid_queues:
                 if queue != -1:
                     break
             else:
-                if nid not in today_nid:
-                    num_of_del += len(nid_queues)
-                    to_sus_nid.append(nid)
-        self.col.remove_notes(to_sus_nid)
+                num_of_del += len(nid_queues)
+                remove_nids.append(nid)
+        self.col.remove_notes(remove_nids)
         p = subprocess.Popen(["efficiency", "-a", f"{num_of_del}"])
         p.wait()
         if num_of_del:
