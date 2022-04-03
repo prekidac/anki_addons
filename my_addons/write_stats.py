bc26a3a1e1da9a7a7cd33d9a8ec14ee252a4bbd7
slavujko <al.slavisa@gmail.com>
2022-04-03 09:10:23 +0200

    ➕ add(main): correct ratio

--- my_addons/write_stats.py
+++ my_addons/write_stats.py
@@ -8,11 +8,19 @@ def unloadProfileAndExit_wrapper(func) -> callable:
     def wrapper(self):
         stats = {}
         stats["new_cards"] = len(self.col.find_cards("is:new"))
+
         circulation = self.col.db.scalar(
             "select count(id) from cards where reps > 0")
         stats["circulation"] = circulation
+
         sum_reps = self.col.db.scalar("select sum(reps) from cards")
         stats["average_reps"] = round(sum_reps / circulation)
+
+        again = len(self.col.find_cards("rated:1:1"))
+        today = len(self.col.db.list(
+            "select distinct cid from revlog where id > ?", (self.col.sched.dayCutoff-86400)*1000))
+        if today > 0:
+            stats["correct ratio"] = round((today-again)/today,2)
         write_stats(stats)
         return func(self)
     return wrapper
