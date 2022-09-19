e580c7b2e43853b54efa9f67a9d8cf762a28613c
slavujko <al.slavisa@gmail.com>
2022-09-19 15:17:31 +0200

    ➕ add(main): hard card calculation

--- my_addons/suspend_card.py
+++ my_addons/suspend_card.py
@@ -4,9 +4,21 @@ from aqt import gui_hooks
 def suspend(self, card, *args) -> None:
     if card.ivl >= self.mw.col.sched._revConf(card)["maxIvl"]:
         self.mw.col.sched.suspend_cards([self.card.id])
-    elif card.reps >= 15:
-        print("-- Hard card: ", end="")
+    elif card.reps >= num_of_steps(self.mw.col, card):
+        print("-- Hard card")
         self.mw.col.sched.suspend_cards([card.id])
 
+def num_of_steps(col, card) -> int:
+    AGAIN = 10
+    revConf = col.sched._revConf(card)
+    fct = card.factor / 1000
+    newConf = col.sched._newConf(card)
+    new = len(newConf["delays"])
+    review = 0
+    ivl = 1
+    while ivl < revConf["maxIvl"]:
+        review += 1
+        ivl = int(max(ivl * fct, ivl + 2))
+    return AGAIN + new + review
 
 gui_hooks.reviewer_did_answer_card.append(suspend)
