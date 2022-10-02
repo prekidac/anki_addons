bdc8f0be0150c70b3bdabbcd31f1fb302425abb5
slavujko <al.slavisa@gmail.com>
2022-10-02 19:45:59 +0200

    ➕ add(main): notify unusual cards

--- my_addons/answer_modify.py
+++ my_addons/answer_modify.py
@@ -1,12 +1,35 @@
 from aqt import gui_hooks
+import re
 
+def answer(col, card) -> str:
+    nid = col.db.scalar(
+        "select nid from cards where id == ?", card.id)
+    note = col.db.scalar(
+        "select flds from notes where id == ?", nid)
+    note = re.split('\x1f', note)
+    if len(note) > 1:
+        return note[card.ord]
+    else:
+        CHARS = r"([^}]*[}]?[^}]+|[^}]*)"
+        po = re.compile(r"{{c" + str(card.ord + 1) + r"::" + CHARS)
+        match_list = po.findall(note[0])
+        po = re.compile(r"(.*)" + "::")
+        answers = []
+        for i in match_list:
+            if po.search(i):
+                answers.append(po.search(i).group(1))
+            else:
+                answers.append(i)
+        return " ".join(answers)
 
 def easy_new_card(ease_tuple, reviewer, card):
     if ease_tuple[1] > 1 and card.queue == 0:
         # new card and you know it - answer 4
+        print(f"Easy card: {answer(reviewer.mw.col, card)}")
         return (True, 4)
     elif ease_tuple[1] == 1 and card.id in reviewer.mw.col.find_cards("rated:1:1"):
         # fail card only once per day
+        print(f"Failed card: {answer(reviewer.mw.col, card)}")
         return (True, 3)
     return ease_tuple
 
--- my_addons/suspend_card.py
+++ my_addons/suspend_card.py
@@ -1,10 +1,32 @@
 from aqt import gui_hooks
+import re
 
+def answer(col, card) -> str:
+    nid = col.db.scalar(
+        "select nid from cards where id == ?", card.id)
+    note = col.db.scalar(
+        "select flds from notes where id == ?", nid)
+    note = re.split('\x1f', note)
+    if len(note) > 1:
+        return note[card.ord]
+    else:
+        CHARS = r"([^}]*[}]?[^}]+|[^}]*)"
+        po = re.compile(r"{{c" + str(card.ord + 1) + r"::" + CHARS)
+        match_list = po.findall(note[0])
+        po = re.compile(r"(.*)" + "::")
+        answers = []
+        for i in match_list:
+            if po.search(i):
+                answers.append(po.search(i).group(1))
+            else:
+                answers.append(i)
+        return " ".join(answers)
 
 def suspend(self, card, *args) -> None:
     if card.ivl >= self.mw.col.sched._revConf(card)["maxIvl"]:
-        self.mw.col.sched.suspend_cards([self.card.id])
+        self.mw.col.sched.suspend_cards([card.id])
     elif card.reps >= num_of_steps(self.mw.col, card):
+        print(f"Hard card: {answer(self.mw.col, card)}")
         self.mw.col.sched.suspend_cards([card.id])
 
 def num_of_steps(col, card) -> int:
