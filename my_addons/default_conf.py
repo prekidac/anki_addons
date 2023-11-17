3e7a615936a2f443fe1e23dd55310ba85741a3fa
slavujko <al.slavisa@gmail.com>
2023-11-17 22:45:36 +0100

    🔧 change(main): perday 100

--- my_addons/default_conf.py
+++ my_addons/default_conf.py
@@ -10,7 +10,7 @@ defaultConf = {
         "initialFactor": 1500,
         "separate": True,
         "order": NEW_CARDS_DUE,
-        "perDay": 10,
+        "perDay": 100,
         # may not be set on old decks
         "bury": True,
     },
