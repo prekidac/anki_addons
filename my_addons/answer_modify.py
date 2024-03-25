259a3907dcd8f7d3fdea71e2a9ff0b674f45043a
slavujko <al.slavisa@gmail.com>
2024-03-25 11:49:33 +0100

    🔧 change(main): answer modify print change

--- my_addons/answer_modify.py
+++ my_addons/answer_modify.py
@@ -3,26 +3,6 @@ import re
 from aqt import mw
 config = mw.addonManager.getConfig(__name__)
 
-def answer(col, card) -> str:
-    nid = col.db.scalar(
-        "select nid from cards where id == ?", card.id)
-    note = col.db.scalar(
-        "select flds from notes where id == ?", nid)
-    note = re.split('\x1f', note)
-    if len(note) > 1:
-        return note[card.ord]
-    else:
-        CHARS = r"([^}]*[}]?[^}]+|[^}]*)"
-        po = re.compile(r"{{c" + str(card.ord + 1) + r"::" + CHARS)
-        match_list = po.findall(note[0])
-        po = re.compile(r"(.*)" + "::")
-        answers = []
-        for i in match_list:
-            if po.search(i):
-                answers.append(po.search(i).group(1))
-            else:
-                answers.append(i)
-        return " ".join(answers)
 
 def easy_new_card(ease_tuple, reviewer, card):
     if config["hard_and_easy_remove"]:
@@ -32,11 +12,11 @@ def easy_new_card(ease_tuple, reviewer, card):
             ease_tuple = (True, 3)
     if ease_tuple[1] > 1 and card.queue == 0 and config["easy_new_card"]:
         # new card and you know it - answer 4
-        print(f"Easy card: {answer(reviewer.mw.col, card)}")
+        print(f"Easy card: {card.id}")
         return (True, 4)
     elif ease_tuple[1] == 1 and card.id in reviewer.mw.col.find_cards("rated:1:1") and config["fail_only_once"]:
         # fail card only once per day
-        print(f"Failed card: {answer(reviewer.mw.col, card)}")
+        print(f"Failed card: {card.id}")
         return (True, 3)
     return ease_tuple
 
