d4fdf95609949e4b91cc61b457092a5310c5fd5f
slavujko <al.slavisa@gmail.com>
2024-03-27 10:52:47 +0100

    🌱 rework(main): suspend card

--- my_addons/answer_modify.py
+++ my_addons/answer_modify.py
@@ -1,5 +1,4 @@
 from aqt import gui_hooks
-import re
 from aqt import mw
 config = mw.addonManager.getConfig(__name__)
 
--- my_addons/suspend_card.py
+++ my_addons/suspend_card.py
@@ -1,43 +1,16 @@
 from aqt import gui_hooks
-import re
-import subprocess
 from aqt import mw
 config = mw.addonManager.getConfig(__name__)
 
-def answer(col, card) -> str:
-    nid = col.db.scalar(
-        "select nid from cards where id == ?", card.id)
-    note = col.db.scalar(
-        "select flds from notes where id == ?", nid)
-    note = re.split('\x1f', note)
-    if len(note) > 1:
-        return note[card.ord]
-    else:
-        CHARS = r"([^}]*[}]?[^}]+|[^}]*)"
-        po = re.compile(r"{{c" + str(card.ord + 1) + r"::" + CHARS)
-        match_list = po.findall(note[0])
-        po = re.compile(r"(.*)" + "::")
-        answers = []
-        for i in match_list:
-            if po.search(i):
-                answers.append(po.search(i).group(1))
-            else:
-                answers.append(i)
-        return " ".join(answers)
 
 def suspend(self, card, *args) -> None:
     hc = 0
     if card.ivl >= config["maximum_interval"]:
         self.mw.col.sched.suspend_cards([card.id])
     elif card.reps >= num_of_steps(self.mw.col, card):
-        print(f"Hard card: {answer(self.mw.col, card)}")
+        print(f"Hard card: {card.id}")
         hc = 1
         self.mw.col.sched.suspend_cards([card.id])
-    try:
-        p = subprocess.Popen(["hard_card", "-a", f"{hc}"])
-        p.wait()
-    except:
-        print("No hard_card app")
 
 def num_of_steps(col, card) -> int:
     AGAIN = config["hard_card_fail_steps"]
