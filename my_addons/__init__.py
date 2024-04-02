13c575c586173991fb69a77d0836284a3eca0cbb
slavujko <al.slavisa@gmail.com>
2024-04-02 08:03:04 +0200

    ➖ remove(main): write stats

--- my_addons/__init__.py
+++ my_addons/__init__.py
@@ -5,6 +5,4 @@ from . import default_conf
 from . import limit             # limit learning to "new_card" number of failed cards
 from . import auto_rate_typed_answer
 from . import suspend_card      # suspend card if reached max_interval
-if os.name == "posix":
-    from . import write_stats 
 from . import answer_modify     # fail card only once per day, easy card
